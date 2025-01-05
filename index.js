54206adf3eab88b15e91253cab87a7843f41496d
johnbbarreda <johnbbarreda@gmail.com>
2025-01-05 19:42:03 +0800

    Enhance code

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 // Function to request fullscreen when the video container is clicked
 function requestFullscreen(element) {
-    var iframe = element.querySelector('iframe');
+    const iframe = element.querySelector('iframe');
     
-    // Request fullscreen on the iframe element
-    if (iframe.requestFullscreen) {
-        iframe.requestFullscreen();
-    } else if (iframe.mozRequestFullScreen) { // Firefox
-        iframe.mozRequestFullScreen();
-    } else if (iframe.webkitRequestFullscreen) { // Chrome, Safari and Opera
-        iframe.webkitRequestFullscreen();
-    } else if (iframe.msRequestFullscreen) { // IE/Edge
-        iframe.msRequestFullscreen();
-    }
+    if (iframe) {
+        // Request fullscreen on the iframe element
+        if (iframe.requestFullscreen) {
+            iframe.requestFullscreen();
+        } else if (iframe.mozRequestFullScreen) { // Firefox
+            iframe.mozRequestFullScreen();
+        } else if (iframe.webkitRequestFullscreen) { // Chrome, Safari and Opera
+            iframe.webkitRequestFullscreen();
+        } else if (iframe.msRequestFullscreen) { // IE/Edge
+            iframe.msRequestFullscreen();
+        }
 
-    // Autoplay the video when clicked
-    var video = iframe;
-    var autoplayURL = iframe.src.includes("autoplay=1") ? iframe.src : iframe.src + "?autoplay=1";
-    iframe.src = autoplayURL;
+        // Autoplay the video by appending autoplay parameter if not present
+        const src = iframe.src;
+        if (!src.includes('autoplay=1')) {
+            iframe.src = src.includes('?') ? `${src}&autoplay=1` : `${src}?autoplay=1`;
+        }
+    }
 }
+
+// Optional: Add event listeners to video wrappers for better accessibility
+document.querySelectorAll('.video-wrapper').forEach(wrapper => {
+    wrapper.addEventListener('keydown', (e) => {
+        if (e.key === 'Enter') {
+            requestFullscreen(wrapper);
+        }
+    });
+});
