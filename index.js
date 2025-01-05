8e9e6949cce17d7b6064bb946c5e303dd36007f9
johnbbarreda <johnbbarreda@gmail.com>
2025-01-05 19:30:06 +0800

    Update videos

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-console.log("Hi Github!");
+// Function to request fullscreen when the video container is clicked
+function requestFullscreen(element) {
+    var iframe = element.querySelector('iframe');
+    
+    // Request fullscreen on the iframe element
+    if (iframe.requestFullscreen) {
+        iframe.requestFullscreen();
+    } else if (iframe.mozRequestFullScreen) { // Firefox
+        iframe.mozRequestFullScreen();
+    } else if (iframe.webkitRequestFullscreen) { // Chrome, Safari and Opera
+        iframe.webkitRequestFullscreen();
+    } else if (iframe.msRequestFullscreen) { // IE/Edge
+        iframe.msRequestFullscreen();
+    }
 
+    // Autoplay the video when clicked
+    var video = iframe;
+    var autoplayURL = iframe.src.includes("autoplay=1") ? iframe.src : iframe.src + "?autoplay=1";
+    iframe.src = autoplayURL;
+}
